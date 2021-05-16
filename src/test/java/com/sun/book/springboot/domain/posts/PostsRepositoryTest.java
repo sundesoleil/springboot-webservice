c9df79d3e8af80df119ce78b9a1e9097b8b89f59
sundesoleil <dxrans@gmail.com>
2021-05-16 18:39:51 +0900

    spring jpa test

--- src/test/java/com/sun/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/sun/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,4 +1,46 @@
 package com.sun.book.springboot.domain.posts;
 
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // 다른 설정 없이 H2 DB 자동 실행
 public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After // Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드 지정
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() // insert/update query execute > id 값이 있다면 update, 없으면 insert
+                .title(title)
+                .content(content)
+                .author("sunaimeles@gmail.com")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll(); // 모든 데이터 조회
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
 }
