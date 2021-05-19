5228fb41e61022db872451e97e6cd2ba1fac5703
sundesoleil <dxrans@gmail.com>
2021-05-19 17:38:52 +0900

    수정, 조회 기능 추가

--- src/main/java/com/sun/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sun/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.sun.book.springboot.service.posts;
 
+import com.sun.book.springboot.domain.posts.Posts;
 import com.sun.book.springboot.domain.posts.PostsRepository;
+import com.sun.book.springboot.web.dto.PostsResponseDto;
 import com.sun.book.springboot.web.dto.PostsSaveRequestDto;
+import com.sun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
 
--- src/main/java/com/sun/book/springboot/web/PostApiController.java
+++ src/main/java/com/sun/book/springboot/web/PostApiController.java
@@ -1,11 +1,11 @@
 package com.sun.book.springboot.web;
 
 import com.sun.book.springboot.service.posts.PostsService;
+import com.sun.book.springboot.web.dto.PostsResponseDto;
 import com.sun.book.springboot.web.dto.PostsSaveRequestDto;
+import com.sun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor // 생성자로 Bean을 받아 @Autowired와 동일한 효과
 @RestController
@@ -17,4 +17,15 @@ public class PostApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestsDto){
         return postsService.save(requestsDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
 }
