6020362aaf367a675f7ac4de822ce3f16ae9aa22
sundesoleil <dxrans@gmail.com>
2021-05-30 16:04:59 +0900

    조회 기능 추가

--- src/main/java/com/sun/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sun/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.sun.book.springboot.service.posts;
 
 import com.sun.book.springboot.domain.posts.Posts;
 import com.sun.book.springboot.domain.posts.PostsRepository;
+import com.sun.book.springboot.web.dto.PostsListResponseDto;
 import com.sun.book.springboot.web.dto.PostsResponseDto;
 import com.sun.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,6 +38,13 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly=true) // 트랜잭션 범위는 유지하되 조회 기능만 남겨두어 속도 개선
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
 
