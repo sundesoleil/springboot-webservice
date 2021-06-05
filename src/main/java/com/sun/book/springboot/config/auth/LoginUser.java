b25bdefce982be709be61ad83e6bdac0a0743fb0
sundesoleil <dxrans@gmail.com>
2021-06-05 22:37:36 +0900

    @LoginUser 사용을 위한 환경 구성

---
+++ src/main/java/com/sun/book/springboot/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package com.sun.book.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser { // 이 파일을 annotation class로 지정
+}
