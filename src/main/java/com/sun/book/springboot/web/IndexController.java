15d1a0d4275acca54d64f0fd0ad4383ef35232cd
sundesoleil <dxrans@gmail.com>
2021-06-07 09:07:45 +0900

    LoginUserArgumentResolver를 WebMvcConfigurer에 추가

--- src/main/java/com/sun/book/springboot/web/IndexController.java
+++ src/main/java/com/sun/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sun.book.springboot.web;
 
+import com.sun.book.springboot.config.auth.LoginUser;
 import com.sun.book.springboot.config.auth.dto.SessionUser;
 import com.sun.book.springboot.service.posts.PostsService;
 import com.sun.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
