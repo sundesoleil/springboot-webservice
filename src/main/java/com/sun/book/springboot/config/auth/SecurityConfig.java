05c8818e6db141a088511b26318effc7c8d5503b
sundesoleil <dxrans@gmail.com>
2021-06-14 19:45:58 +0900

    HelloContr HelloControllerTest 수정

--- src/main/java/com/sun/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/sun/book/springboot/config/auth/SecurityConfig.java
@@ -14,8 +14,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                csrf().disable()
+        http
+                .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
--- src/test/java/com/sun/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/sun/book/springboot/web/HelloControllerTest.java
@@ -1,19 +1,21 @@
 package com.sun.book.springboot.web;
 
+import com.sun.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.security.access.SecurityConfig;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class,
